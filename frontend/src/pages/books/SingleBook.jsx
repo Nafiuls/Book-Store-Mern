96bfbcb64fe2014cab5f9c82cecaa5001cb365c4
Nafiuls <snafiul700@gmail.com>
2024-11-13 15:31:37 +0600

    firebase setup done in the frontend(config)

--- frontend/src/pages/books/SingleBook.jsx
+++ frontend/src/pages/books/SingleBook.jsx
@@ -2,16 +2,19 @@ import { useParams } from "react-router-dom";
 import { useFetchBookByIdQuery } from "../../redux/features/books/booksApi";
 import { FiShoppingCart } from "react-icons/fi";
 import { getImgUrl } from "../../utils/getImgUrl";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/features/cart/cartSlice";
 
 const SingleBook = () => {
   const { id } = useParams();
-  console.log(id);
   const { data: book, isLoading, isError } = useFetchBookByIdQuery(id);
+  const dispatch = useDispatch();
+  const handleAddToCart = (book) => {
+    dispatch(addToCart(book));
+  };
   if (isLoading) return <h1>Loading...</h1>;
   if (isError) return <h1>Error</h1>;
-  const handleAddToCart = (book) => {
-    console.log(book);
-  };
+
   return (
     <div className="max-w-lg shadow-md p-5">
       <h1 className="text-2xl font-bold mb-6">{book.title}</h1>
@@ -43,7 +46,7 @@ const SingleBook = () => {
 
         <button
           onClick={() => handleAddToCart(book)}
-          className="btn-primary px-6 space-x-1 flex items-center gap-1 "
+          className="bg-primary py-2 rounded hover:bg-black hover:text-white transition-all duration-300 px-6 space-x-1 flex items-center gap-1 "
         >
           <FiShoppingCart className="" />
           <span>Add to Cart</span>
