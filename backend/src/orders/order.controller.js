6f59972959f643ac8d4041e771526c46c1a427ee
Nafiuls <snafiul700@gmail.com>
2024-11-15 17:06:39 +0600

    order apis created

--- backend/src/orders/order.controller.js
+++ backend/src/orders/order.controller.js
@@ -12,6 +12,22 @@ const createOrder = async (req, res) => {
   }
 };
 
+// get order by user email
+const getOrderByEmail = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const orders = await Order.find({ email }).sort({ createdAt: -1 });
+    if (!orders) {
+      return res.status(404).send({ message: "orders not found !" });
+    }
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error("erro fetching order", error);
+    res.status(500).send({ message: "Failed to fetch orders" });
+  }
+};
+
 module.exports = {
   createOrder,
+  getOrderByEmail,
 };
--- backend/src/orders/order.router.js
+++ backend/src/orders/order.router.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createOrder } = require("./order.controller");
+const { createOrder, getOrderByEmail } = require("./order.controller");
 
 const router = express.Router();
 
 // create a order endpoint
 router.post("/create-order", createOrder);
-
+// get order by user email
+router.get("/get-order/:email", getOrderByEmail);
 module.exports = router;
